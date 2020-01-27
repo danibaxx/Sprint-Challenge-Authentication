4dd0fd49af43301022d0064d3ec5ae380d63ca2d
Danielle B <danibaxx@hotmail.com>
2020-01-26 17:52:12 -0700

    created endpoints for register and login, now to code out authorization middleware

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -3,19 +3,47 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 const secrets = require('../config/secrets');
 
+const usersModel = require('../users/users-model');
+
 const router = require('express').Router();
 
 router.post('/register', async (req, res, next) => {
   // implement registration
   try {
-    const saved = await 
+    const saved = await usersModel.add(req.body)
+    res.status(201).json(saved)
   } catch(err) {
-
+    next(err)
   }
 });
 
-router.post('/login', (req, res) => {
+router.post('/login', async (req, res, next) => {
   // implement login
+  try {
+    const { username, password } = req.body;
+    const user = await usersModel.findBy({ username }).first()
+    const pwValid = await bcrypt.compare(password, user.password)
+
+    if (user && pwValid) {
+      const jwtoken = jwt.sign({
+        subject: user.id,
+        username: user.username,
+      }, secrets.jwt, {
+        expiresIn: "14d",
+      })
+
+      res.status(200).json({
+        message: `Welcome ${user.username}`,
+        token: jwtoken,
+      })
+    } else {
+      res.status(401).json({
+        message: "please sign-in!"
+      })
+    }
+  } catch(err) {
+    next(err)
+  }
 });
 
 module.exports = router;
